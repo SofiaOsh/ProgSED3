cc226c32472eae0994abe6ca0e68c1905b94315c
Katya <email>
2022-12-27 11:19:17 +0400

    Добавление переменных и методов в классе документ

--- ProgSed/ProgSED2/ProgSED2/Program.cs
+++ ProgSed/ProgSED2/ProgSED2/Program.cs
@@ -8,7 +8,45 @@ namespace ProgSED2
 {
     class CDocument
     {
+        protected string Name;
+        protected string Status;
+        protected string Type;
+        protected string Author_1;
+        protected string Author_2;
+        protected string Admin;
+        protected string Number;
+        protected string Comments;
+        protected double Summa;
+        protected DateTime Date_Start;
+        protected DateTime Date_Finish;
 
+        CDocument()
+        {
+            Name = Status = Type = Author_1 = Author_2 = Admin = Number = Comments = "";
+            Summa = 0.0;
+            var Date_Start = new DateTime(1, 1, 1900);
+            var Date_Finish = new DateTime(1, 1, 9999);
+        }
+        CDocument(string na, string st, string ty, string a1, string a2, string ad, string nu, string co, double su, DateTime ds, DateTime df)
+        {
+            Name = na; Status = st; Type = ty; Author_1 = a1; Author_2 = a2; Admin = ad; Number = nu; Comments = co;
+            Summa = su;
+            Date_Start = ds;
+            Date_Finish = df; 
+        }
+
+        public void Edit()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+        public void Watch()
+        {
+
+        }
     }
     static class Program
     {
